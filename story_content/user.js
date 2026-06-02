06d89818cdb46e02b035c128504470a6acd7d44e
DS <produkmediarsn@gmail.com>
2026-06-02 16:36:56 +0700

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,355 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5wPwnhKhTTA":
+        Script1();
+        break;
+      case "5hLbuaunWhT":
+        Script2();
+        break;
+      case "61IXgLwuKSh":
+        Script3();
+        break;
+      case "5x5VZflXzjp":
+        Script4();
+        break;
+      case "6oZMSu1Q55F":
+        Script5();
+        break;
+      case "5V0mbQ98GPN":
+        Script6();
+        break;
+      case "6hjraBv4S6r":
+        Script7();
+        break;
+      case "5tt52SjRhMT":
+        Script8();
+        break;
+      case "6HP0ddJhaEb":
+        Script9();
+        break;
+      case "6C4z9P9iTuE":
+        Script10();
+        break;
+      case "6b4ASUEuQZM":
+        Script11();
+        break;
+      case "6MzSbpxzA8i":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script2()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script3()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script4()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script5()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script6()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script7()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script8()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script9()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script10()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script11()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
+function Script12()
+{
+  if (!window.bgMusic) {
+    // TRIK SAKTI: Menambahkan angka unik (timestamp) di belakang nama file 
+    // biar browser dipaksa download file baru dari GitHub, bukan dari cache.
+    var versiBaru = "?v=" + new Date().getTime(); 
+    
+    window.bgMusic = new Audio("bgm.mp3" + versiBaru); 
+    window.bgMusic.loop = true; 
+    window.bgMusic.volume = 0.5; 
+    
+    // Jalankan musik
+    window.bgMusic.play().then(function() {
+        console.log("Autoplay sukses!");
+    }).catch(function(error) {
+        console.log("Autoplay diblokir. Menunggu klik pertama...");
+        
+        // Nunggu klik pertama di layar
+        document.addEventListener('click', function playOnFirstClick() {
+            window.bgMusic.play();
+            document.removeEventListener('click', playOnFirstClick);
+        }, { once: true });
+    });
+}
+}
+
